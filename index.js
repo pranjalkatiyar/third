c3e086d33bb0b323f1d415e9cf6d172474ee05dc
pranjalkatiyar <76843696+pranjalkatiyar@users.noreply.github.com>
2021-05-16 22:20:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+function profile1() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+    
+  }
+}
+  
+  function profile2() {
+    var dots = document.getElementById("empty");
+    var moreText = document.getElementById("most");
+    var btnText = document.getElementById("btn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function Services1() {
+    var dots = document.getElementById("doots");
+    var moreText = document.getElementById("moore");
+    var btnText = document.getElementById("myBton");
+    var content = document.getElementById("content");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Purpose & Strategy"; 
+      moreText.style.display = "none";
+     content.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Purpose & Strategy"; 
+      moreText.style.display = "inline";
+      content.style.display = "inline";
+    }
+  }
+  function Services1() {
+    var dots = document.getElementById("doots");
+    var moreText = document.getElementById("moore");
+    var btnText = document.getElementById("myBton");
+    var content = document.getElementById("content");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Purpose & Strategy";
+        moreText.style.display = "none";
+        content.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Purpose & Strategy";
+        moreText.style.display = "inline";
+        content.style.display = "inline";
+    }
+}
+
+function Services2() {
+    var dots = document.getElementById("dooots");
+    var moreText = document.getElementById("mooore");
+    var btnText = document.getElementById("myBtoon");
+    var content = document.getElementById("coontent");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Value & Growth";
+        moreText.style.display = "none";
+        content.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Value & Growth";
+        moreText.style.display = "inline";
+        content.style.display = "inline";
+    }
+}
+
+function Services3() {
+    var dots = document.getElementById("doooots");
+    var moreText = document.getElementById("moooore");
+    var btnText = document.getElementById("myBtooon");
+    var content = document.getElementById("cooontent");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Leadership & Culture";
+        moreText.style.display = "none";
+        content.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Leadership & Culture";
+        moreText.style.display = "inline";
+        content.style.display = "inline";
+    }
+}
